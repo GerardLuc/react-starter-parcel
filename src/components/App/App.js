2cfe0fc31b756d30c1cdef4b8b4c4ab6af85ba37
Hippo203 <hippolyte.drouet@eemi.com>
2019-03-14 18:12:43 +0100

    ShifuHippo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,34 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import CounterContainer from '../Counter/CounterContainer';
-import Hangman from '../Hangman/Hangman';
-import Shifumi from '../Shifumi/Shifumi';
 import Home from '../Home/Home';
+import Hangman from '../hangman/Hangman';
+import ShifumiContainer from '../shifumi/ShifumiContainer';
 
 const App = () => (
   <Router>
     <div className="container">
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/counter">Counter</Link>
-          </li>
-          <li>
-            <Link to="/hangman">Hangman</Link>
-          </li>
-          <li>
-            <Link to="/shifumi">Shifumi</Link>
-          </li>
-        </ul>
+        <Link to="/">Home</Link>
+        <Link to="/counter">Counter</Link>
+        <Link to="/shifumi">Shifumi</Link>
+        <Link to="/hangman">Hangman</Link>
       </nav>
       <div className="App">
         <Route exact path="/" component={Home} />
         <Route path="/counter" component={CounterContainer} />
         <Route path="/hangman" component={Hangman} />
-        <Route path="/shifumi" component={Shifumi} />
+        <Route path="/shifumi" component={ShifumiContainer} />
       </div>
     </div>
   </Router>
--- src/components/Shifumi/ShifumiContainer.js
+++ src/components/Shifumi/ShifumiContainer.js
@@ -2,26 +2,64 @@ import React, { Component } from 'react';
 import Shifumi from './Shifumi';
 
 class ShifumiContainer extends Component {
-  state = {
-    score: 0,
-  };
+state ={
+    score1: 0,
+    score2: 0
+};
 
-  // Conception développer le jeu pierre feuille ciseau
-  // 3 boutons ou bien un select et un bouton Play
+handleClick = event =>{
+    this.play(event.currentTarget.value);
+}
 
-  // Afficher un score, premier arrivé à 3 a gagné.
-  // ['pierre', 'feuille', 'ciseaux'];
-  // Math.random() / index ?
-
-  // play(){}
-  // handleClick = event => {}
+play(choice){
+    const comp = Math.floor(Math.random() * 3);
+    if(choice == comp){
+        return;
+    }
+    if(choice == 0 && comp == 1){
+        this.setState(prevState => ({
+            score2: prevState.score2 + 1,
+          }));
+    }
+    if(choice == 0 && comp == 2){
+        this.setState(prevState => ({
+            score1: prevState.score1 + 1,
+          }));
+    }
+    if(choice == 1 && comp == 0){
+        this.setState(prevState => ({
+            score1: prevState.score1 + 1,
+          }));
+    }
+    if(choice == 1 && comp == 2){
+        this.setState(prevState => ({
+            score2: prevState.score2 + 1,
+          }));
+    }
+    if(choice == 2 && comp == 0){
+        this.setState(prevState => ({
+            score2: prevState.score2 + 1,
+          }));
+    }
+    if(choice == 2 && comp == 1){
+        this.setState(prevState => ({
+            score1: prevState.score1 + 1,
+          }));
+    }
+    if(this.state.score1 == 3 || this.state.score2 == 3){
+        this.setState ({
+            score1 :0,
+            score2 :0
+          });
+    }
+}
 
   render() {
-    const { score } = this.state;
-
-    // Passer handleClick en props à <Shifumi />
-    return <Shifumi score={score} />;
+    const { score1, score2 } = this.state;
+    return <Shifumi score1={score1} score2={score2} handleClick={this.handleClick} />;
   }
 }
 
 export default ShifumiContainer;
+
+// Merci à Balthazar Pelletier pour l'aide apportée
