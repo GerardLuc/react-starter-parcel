dfb074fa959b5ba4bbfd7815c17c906212a209c7
David V <david@venin.pro>
2019-03-14 12:50:22 +0100

    update appcontainer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import CounterContainer from '../Counter/CounterContainer';
+//
+//
 import Home from '../Home/Home';
 
 const App = () => (
@@ -13,6 +15,8 @@ const App = () => (
       <div className="App">
         <Route exact path="/" component={Home} />
         <Route path="/counter" component={CounterContainer} />
+        {/*  */}
+        {/*  */}
       </div>
     </div>
   </Router>
