2817bf71e034100c1ad4c1c2d5297c4e041b4679
David V <david@venin.pro>
2019-03-14 14:52:16 +0100

    add hangman / shifumi view

---
+++ src/components/Shifumi/Shifumi.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Shifumi = () => (
+  <div>
+    <h2>Let's play Shifumi</h2>
+  </div>
+);
+
+export default Shifumi;
---
+++ src/components/Shifumi/ShifumiContainer.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Shifumi from './Shifumi';
+
+class ShifumiContainer extends Component {
+  state = {
+    score: 0,
+  };
+
+  // Conception développer le jeu pierre feuille ciseau
+  // 3 boutons ou bien un select et un bouton Play
+
+  // Afficher un score, premier arrivé à 3 a gagné.
+  // ['pierre', 'feuille', 'ciseaux'];
+  // Math.random() / index ?
+
+  // play(){}
+  // handleClick = event => {}
+
+  render() {
+    const { score } = this.state;
+
+    // Passer handleClick en props à <Shifumi />
+    return <Shifumi score={score} />;
+  }
+}
+
+export default ShifumiContainer;
