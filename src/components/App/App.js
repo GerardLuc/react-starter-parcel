6d1b1bf0936870799e7e6932817c413cf6e19a1d
David V <david@venin.pro>
2019-03-15 09:12:01 +0100

    starter clean with router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import CounterContainer from '../Counter/CounterContainer';
-import Hangman from '../Hangman/Hangman';
-import Shifumi from '../Shifumi/Shifumi';
 import Home from '../Home/Home';
 
 const App = () => (
@@ -13,22 +10,10 @@ const App = () => (
           <li>
             <Link to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/counter">Counter</Link>
-          </li>
-          <li>
-            <Link to="/hangman">Hangman</Link>
-          </li>
-          <li>
-            <Link to="/shifumi">Shifumi</Link>
-          </li>
         </ul>
       </nav>
       <div className="App">
         <Route exact path="/" component={Home} />
-        <Route path="/counter" component={CounterContainer} />
-        <Route path="/hangman" component={Hangman} />
-        <Route path="/shifumi" component={Shifumi} />
       </div>
     </div>
   </Router>
--- src/components/Hangman/Hangman.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Hangman = () => (
-  <div>
-    <h2>Let's play Hangman</h2>
-  </div>
-);
-
-export default Hangman;
